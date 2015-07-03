42b3bf862212221f58c5fd1a4818c2ed662a1a6d
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-07-03 17:07:00 +0200

    Update blast_script.php

--- newsletter/blast_script.php
+++ newsletter/blast_script.php
@@ -15,17 +15,17 @@ while($row = mysql_fetch_array($sql)){
 	
 	$mail_body = '<html>
 <body style="background-color:#CCC; color:#000; font-family: Arial, Helvetica, sans-serif; line-height:1.8em;">
-<h3><a href="http://www.developphp.com"><img src="http://www.yoursite.com/images/logo.png" alt="DevelopPHP" width="216" height="36" border="0"></a> Newsletter
+<h3><a href="https://bit.ly/Suriyaa"><img src="{{ site.url }}assets/img/logo.png" alt="Suriyaa Kudo" width="216" height="36" border="0"></a> Newsletter
 </h3>
 <p>Hello ' . $name . ',</p>
 <p>You can make this out to be just like most any web page or design format you require using HTML and CSS.</p>
-<p>~Adam @ DevelopPHP</p>
+<p>~Suriyaa Kudo</p>
 <hr>
-<p>To opt out of receiving this newsletter,  <a href="http://www.developphp.com/Tests/newsletter/optout.php?e=' . $email . '">click here</a> and we will remove you from the listing immediately.</p>
+<p>To opt out of receiving this newsletter,  <a href="{{ site.url }}newsletter/optout.php?e=' . $email . '">click here</a> and I will remove you from the listing immediately.</p>
 </body>
 </html>';
-    $subject = "Develop PHP Newsletter";
-    $headers  = "From:newsletter@developphp.com\r\n";
+    $subject = "Suriyaa Kudo Newsletter";
+    $headers  = "From:newsletter@suriyaa.isc\r\n";
     $headers .= "Content-type: text/html\r\n";
     $to = "$email";
 
@@ -48,7 +48,7 @@ if ($numRows == 0) { // $numRows is set on line 4 using the existing query
 	 
 	 $subj = "Newsletter Campaign Has Ended";
 	 $body = "The current newsletter campaign has ended. All have been sent the newsletter.";
-     $hdr  = "From:newsletter@developphp.com\r\n";
+     $hdr  = "From:newsletter@suriyaa.isc\r\n";
      $hdr .= "Content-type: text/html\r\n";
      mail("yourEmailAddressHere", $subj, $body, $hdr);
 	
