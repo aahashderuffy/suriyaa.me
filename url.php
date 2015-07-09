39b1006fcc1d1f35729996f45dba10055febd2ec
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-07-09 10:08:42 +0200

    Create url.php

---
+++ url.php
@@ -0,0 +1,17 @@
+<?php
+
+ob_start(); // ensures anything dumped out will be caught
+
+// do stuff here
+$url = 'https://bit.ly/Suriyaa'; // this can be set based on whatever
+
+// clear out the output buffer
+while (ob_get_status()) 
+{
+    ob_end_clean();
+}
+
+// no redirect
+header( "Location: $url" );
+
+?>
