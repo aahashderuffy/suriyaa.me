d10ae15608e01000a294d7e3857516a797213af5
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-09-18 16:13:50 +0200

    Create backgroundservice.js

---
+++ assets/js/preload/backgroundservice.js
@@ -0,0 +1,8 @@
+/* 
+ * BackgroundService
+ * Creates a web worker for loading website contents in the background.
+ * Copyright (c) 2015-present Suriyaa Kudo
+ * https://bit.ly/Suriyaa | @SuriyaaKudoIsc
+ * Written by Suriyaa Kudo
+ */
+var BackgroundService = new
